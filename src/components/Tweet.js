ea70f0d9ef43cf215a13aaba5bf1c2d62db2e5b3
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2020-10-29 20:42:36 +0100

    :lipstick: better Tweet style

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,43 +1,39 @@
 //React Bootstrap
-import { Media, Image, Row, Col } from 'react-bootstrap';
+import { Media, Image, Row } from 'react-bootstrap';
 
 const Tweet = ({ tweet }) => {
   return (
     <Media>
       <Media.Body className="mx-2">
         <Row>
-          <Col>
-            <a
-              href={`https://twitter.com/${tweet.username}`}
-              target="_blank noreferrer"
-            >
-              <h3 className="float-top">{tweet.name}</h3>
-            </a>
-          </Col>
-          <Col>
-            <h6 className="float-right float-top">{`@${tweet.username}`}</h6>
-          </Col>
+          <a
+            href={`https://twitter.com/${tweet.username}`}
+            target="_blank noreferrer"
+          >
+            <h3 className="align-text-bottom">{tweet.name}</h3>
+          </a>
+
+          <h6 className="ml-auto align-text-bottom">{tweet.datetime}</h6>
+        </Row>
+        <Row>
+          <h6 className="ml-3 align-text-bottom">{`@${tweet.username}`}</h6>
         </Row>
         <Row>
           <p>{tweet.tweet}</p>
         </Row>
         <Row>
           {tweet.photos.length > 0
-            ? tweet.photos.map((photo) => <Image src={photo} thumbnail />)
+            ? tweet.photos.map((photo) => <Image src={photo} rounded fluid />)
             : null}
         </Row>
         <Row>
-          <Col>
-            <a
-              href={`https://twitter.com/${tweet.username}/status/${tweet.conversation_id}`}
-              target="_blank noreferrer"
-            >
-              Link to tweet
-            </a>
-          </Col>
-          <Col>
-            <h6 className="float-right">{tweet.datetime}</h6>
-          </Col>
+          <a
+            href={`https://twitter.com/${tweet.username}/status/${tweet.conversation_id}`}
+            target="_blank noreferrer"
+          >
+            <i class="fas fa-link"></i>
+            Link to tweet
+          </a>
         </Row>
       </Media.Body>
     </Media>
