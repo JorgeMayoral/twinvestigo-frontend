b2ec4863d8f1f4c0c6160b48e98f0f9b7cd54703
Jorge Mayoral Álvarez <y0rch.developer@gmail.com>
2020-11-02 21:41:18 +0100

    :iphone: Solved problem with links not showing in navbar on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,33 @@ const Header = ({ contactButton }) => {
   return (
     <Navbar
       bg="primary"
-      variant="primary"
+      variant="light"
       expand="lg"
       collapseOnSelect
       sticky="top"
     >
       <Container>
         <Navbar.Brand>Twinvestigo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <a
+            <Nav.Link
               href="https://jorgemayoral.com/twinvestigo"
               target="_blank noreferrer"
             >
               <Button variant="link" style={{ color: 'black' }}>
                 About
               </Button>
-            </a>
-            <Button
-              variant="link"
-              style={{ color: 'black' }}
-              onClick={contactButton}
-            >
-              Contact
-            </Button>
+            </Nav.Link>
+            <Nav.Link>
+              <Button
+                variant="link"
+                style={{ color: 'black' }}
+                onClick={contactButton}
+              >
+                Contact
+              </Button>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
